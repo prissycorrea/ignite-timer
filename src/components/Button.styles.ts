901b488aebdcb61365c8976d0954b41963d1ef93
prissycorrea <pris.pcd@gmail.com>
2024-09-09 18:26:16 -0300

    cores e fontes globais

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,12 +17,12 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps> `
     width: 100px;
     height: 50px;
-    border-radius: 8px;
+    border-radius: 4px;
     border: none;
     margin: 8px;
     cursor: pointer;
 
-    background-color: ${props => props.theme.primary};
+    background-color: ${props => props.theme['green-500']};
     color: ${props => props.theme.white};
     /*${props => {
         return `background-color: ${buttonVariants[props.variant || 'primary']};`
