4b5205326ef1497690c0ae51924d0af33924cd71
prissycorrea <pris.pcd@gmail.com>
2024-09-11 10:46:23 -0300

    header e layout

--- src/layouts/DefaultLayouts/index.tsx
+++ src/layouts/DefaultLayouts/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header";
+import { LayoutContainer } from "./styles";
+import { Header } from "../../components/Header/index.tsx";
 
 export function DefaultLayout() {
     return (
-        <div>
+        <LayoutContainer>
            <Header />
            <Outlet /> {/*outlet serve para renderizar o componente filho, neste caso, o conteudo da pagina*/}
-        </div>
+        </LayoutContainer>
     )
 }
