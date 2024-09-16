b79d7765f8fceb182ff5646016f2a5c78a389844
prissycorrea <pris.pcd@gmail.com>
2024-09-16 09:56:58 -0300

    validando formularios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,24 @@
 import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from 'zod';
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa que você irá realizar'),
+    minutesAmount: zod.number().min(5, 'O ciclo precisa ser de no mínimo 5 minutos.').max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+}) 
 
 export function Home() {
-    const { register, handleSubmit, watch } = useForm();
+    const { register, handleSubmit, watch, formState } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    });
 
     function handleCreateNewCycle(data: any) {
         console.log(data);
     }
+
+    console.log(formState.errors);
 
     const task = watch('task');  // Observa o campo task
     const isSubmitDisabled = !task;  // Desabilita o botão de submit se o campo task estiver vazio
